f4a8618c9ea930f258ae4ae52dd105b044aef19f
jiaxianyang <jiaxianyang@jd.com>
2021-01-23 10:10:21 +0800

    修改零移动的方法，更易理解

--- src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[283]移动零.java
+++ src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[283]移动零.java
@@ -20,11 +20,11 @@ package com.rj.leetcode_solution.leetcode.editor.cn;
 class T283 {
 
     public void moveZeroes(int[] nums) {
-        int j = 0;
+       int j = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
-                nums[j] = nums[i];
                 if (i != j) {
+                    nums[j] = nums[i];
                     nums[i] = 0;
                 }
                 j++;
