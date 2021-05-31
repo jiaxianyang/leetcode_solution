5be44670f984c42d0ce87b32f17d66cdf284e0e3
jiaxianyang <jiaxianyang@jd.com>
2021-05-31 16:25:10 +0800

    121 买卖股票的最佳时间

--- src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[121]买卖股票的最佳时机.java
+++ src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[121]买卖股票的最佳时机.java
@@ -74,34 +74,52 @@ class P121BestTimeToBuyAndSellStock {
 
             //3. 动态规划
 
-            int length = prices.length;
-            if (length < 2) {
+//            int length = prices.length;
+//            if (length < 2) {
+//                return 0;
+//            }
+//            int[][] dp = new int[length][2];
+//
+//            //dp[i][0]：规定了今天不持股，有以下两种情况：
+//                //昨天不持股，今天什么都不做；
+//                //昨天持股，今天卖出股票（现金数增加），
+//
+//            //dp[i][1]：规定了今天持股，有以下两种情况：
+//                  //昨天持股，今天什么都不做（现金数与昨天一样）；
+//                  //昨天不持股，今天买入股票（注意：只允许交易一次，因此手上的现金数就是当天的股价的相反数）。
+//
+////            dp[i][0] 下标为 i 这天结束的时候， 不持股，手上拥有的现金数
+////            dp[i][1] 下标为 i 这天结束的时候， 持股，手上拥有的现金数
+//
+//            //初始化：不持股显然为0 ，持股就需要减去第一天（下标为 0）的股价
+//            dp[0][0] = 0;
+//            dp[0][1] = -prices[0];
+//            //从第2天开始便利
+//            for (int i = 1; i < length; i++) {
+//                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+//                dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+//            }
+//            return dp[length - 1][0];
+//        }
+
+            //动态规划优化   我们可以发现dp[i]只和前面一天的状态有关系，所以我们可以进行空间优化。
+            int len = prices.length;
+            if (len <= 1) {
                 return 0;
             }
-            int[][] dp = new int[length][2];
-
-            //dp[i][0]：规定了今天不持股，有以下两种情况：
-                //昨天不持股，今天什么都不做；
-                //昨天持股，今天卖出股票（现金数增加），
-
-            //dp[i][1]：规定了今天持股，有以下两种情况：
-                  //昨天持股，今天什么都不做（现金数与昨天一样）；
-                  //昨天不持股，今天买入股票（注意：只允许交易一次，因此手上的现金数就是当天的股价的相反数）。
-
-//            dp[i][0] 下标为 i 这天结束的时候， 不持股，手上拥有的现金数
-//            dp[i][1] 下标为 i 这天结束的时候， 持股，手上拥有的现金数
-
-            //初始化：不持股显然为0 ，持股就需要减去第一天（下标为 0）的股价
-            dp[0][0] = 0;
-            dp[0][1] = -prices[0];
-            //从第2天开始便利
-            for (int i = 1; i < length; i++) {
-                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
-                dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+            int[] dp = new int[2];
+            //第一天 不持有 、 持有
+            dp[0] = 0;
+            dp[1] = -prices[0];
+            for (int i = 1; i < prices.length; i++) {
+                //不持有 前一天不持有 或者 今天卖了 的最大值
+                dp[0] = Math.max(dp[0], dp[1] + prices[i]);
+                //持有  昨天持有 或者 今天卖入了
+                dp[1] = Math.max(dp[1], -prices[i]);
             }
-            return dp[length - 1][0];
+            //返回不持有的最大值
+            return dp[0];
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
-
 }
