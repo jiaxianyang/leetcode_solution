262e07a2fc8359c272e3b72f0f943693c754a97b
jiaxianyang <jiaxianyang@jd.com>
2021-06-10 22:44:47 +0800

    [88]合并俩个有序数组

--- src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[88]合并两个有序数组.java
+++ src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[88]合并两个有序数组.java
@@ -73,56 +73,34 @@ class P88MergeSortedArray {
 //            }
 
             //3.逆向双指针
-            int p1 = m - 1, p2 = n - 1;
-            int tail = m + n - 1;
-            int cur;
-            while (p1 > -1 || p2 > -1) {
-                if (p1 == -1) {
-                    cur = nums2[p2--];
-                } else if (p2 == -1) {
-                    cur = nums1[p1--];
-                } else if (nums1[p1] < nums2[p2]) {
-                    cur = nums2[p2--];
-                } else {
-                    cur = nums1[p1--];
-                }
-                nums1[tail--] = cur;
-            }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //            int p1 = m - 1, p2 = n - 1;
 //            int tail = m + n - 1;
 //            int cur;
-//            while (p1 >= 0 || p2 >= 0) {
+//            while (p1 > -1 || p2 > -1) {
 //                if (p1 == -1) {
 //                    cur = nums2[p2--];
 //                } else if (p2 == -1) {
 //                    cur = nums1[p1--];
-//                } else if (nums1[p1] > nums2[p2]) {
-//                    cur = nums1[p1--];
-//                } else {
+//                } else if (nums1[p1] < nums2[p2]) {
 //                    cur = nums2[p2--];
+//                } else {
+//                    cur = nums1[p1--];
 //                }
 //                nums1[tail--] = cur;
 //            }
+
+            //4
+            int len1 = m - 1;
+            int len2 = n - 1;
+            int len = m + n - 1;
+            while(len1 >= 0 && len2 >= 0) {
+                // 注意--符号在后面，表示先进行计算再减1，这种缩写缩短了代码
+                nums1[len--] = nums1[len1] > nums2[len2] ? nums1[len1--] : nums2[len2--];
+            }
+            // 表示将nums2数组从下标0位置开始，拷贝到nums1数组中，从下标0位置开始，长度为len2+1
+            System.arraycopy(nums2, 0, nums1, 0, len2 + 1);
+//
+
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
