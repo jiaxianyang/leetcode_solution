267e39ee340d6d823d49de623004cd913974120d
jiaxianyang <jiaxianyang@jd.com>
2021-08-21 17:16:18 +0800

    [84]柱状图中最大的矩形

--- src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[84]柱状图中最大的矩形.java
+++ src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[84]柱状图中最大的矩形.java
@@ -27,13 +27,12 @@ package com.rj.leetcode_solution.leetcode.editor.cn;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.Stack;
 
 //java:柱状图中最大的矩形
 class P84LargestRectangleInHistogram {
     public static void main(String[] args) {
         Solution solution = new P84LargestRectangleInHistogram().new Solution();
-        int[] heights = new int[]{2,1,5,6,2,3};
+        int[] heights = new int[]{2, 1, 5, 6, 2, 3};
         int area = solution.largestRectangleArea(heights);
         System.out.println(area);
     }
@@ -41,23 +40,25 @@ class P84LargestRectangleInHistogram {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int largestRectangleArea(int[] heights) {
-//            1. 暴力方法 ： 枚举宽度
-//            int length = heights.length;
-//            int ans = 0;
-//            for (int left = 0; left < length; left++) {
+            //1. 暴力方法：枚举宽度
+//            int len = heights.length;
+//            int area = 0;
+//            for (int left = 0; left < len; left++) {
+//                //从左开始从新计算最小高度
 //                int minHeight = Integer.MAX_VALUE;
-//                for (int right = left; right < length; right++) {
+//                for (int right = left; right < len; right++) {
 //                    minHeight = Math.min(minHeight, heights[right]);
-//                    ans = Math.max(ans, (right - left + 1) * minHeight);
+//                    area = Math.max(area, (right - left + 1) * minHeight);
 //                }
 //            }
-//            return ans;
+//            return area;
 
+//======================================================================================================================
 
-//            2. 暴力方法： 枚举高度
-//            int length = heights.length;
-//            int ans = 0;
-//            for (int mid = 0; mid < length; mid++) {
+            //2.暴力方法： 枚举高度
+//            int len = heights.length;
+//            int area = 0;
+//            for (int mid = 0; mid < len; mid++) {
 //                //枚举高
 //                int height = heights[mid];
 //                int left = mid;
@@ -66,14 +67,16 @@ class P84LargestRectangleInHistogram {
 //                while (left - 1 >= 0 && heights[left - 1] >= height) {
 //                    --left;
 //                }
-//                while (right + 1 < length && heights[right + 1] >= height) {
+//                while (right + 1 < len && heights[right + 1] >= height) {
 //                    ++right;
 //                }
-//                ans = Math.max(ans, (right - left + 1) * height);
+//                area = Math.max(area, (right - left + 1) * height);
 //            }
-//            return ans;
+//            return area;
 
-            //3.单调栈
+//======================================================================================================================
+
+            //3 思路：后面的柱状图能够先前面的柱状图先算出面积，满足后进先出 想到用使用栈 Stack
 //            int len = heights.length;
 //            if (len == 0) {
 //                return 0;
@@ -82,10 +85,13 @@ class P84LargestRectangleInHistogram {
 //                return heights[0];
 //            }
 //            int area = 0;
+//            //java 推荐stack 使用 ArrayDeque
 //            Deque<Integer> stack = new ArrayDeque<>();
 //            for (int i = 0; i < len; i++) {
+//                //栈顶元素高度大于新栈顶元素， 可计算出面积
 //                while (!stack.isEmpty() && heights[stack.peekLast()] > heights[i]) {
 //                    int height = heights[stack.removeLast()];
+//                    //前面有相同高度的柱子
 //                    while (!stack.isEmpty() && heights[stack.peekLast()] == height) {
 //                        stack.removeLast();
 //                    }
@@ -99,23 +105,27 @@ class P84LargestRectangleInHistogram {
 //                }
 //                stack.addLast(i);
 //            }
-//
+//            //如果栈非空
 //            while (!stack.isEmpty()) {
 //                int height = heights[stack.removeLast()];
+//                //前面有相同高度的柱子
 //                while (!stack.isEmpty() && heights[stack.peekLast()] == height) {
 //                    stack.removeLast();
 //                }
 //                int width;
 //                if (stack.isEmpty()) {
+//                    //高度最低的元素
 //                    width = len;
 //                } else {
+//                    //当前被remove的元素，到后面柱形图没有比它高度低的故用len
 //                    width = len - stack.peekLast() - 1;
 //                }
 //                area = Math.max(area, width * height);
 //            }
 //            return area;
 
-            //4 单调栈，哨兵，空间换时间
+//======================================================================================================================
+            //4.单调栈优化、哨兵(去除空判断，优化代码)、空间换时间
             int len = heights.length;
             if (len == 0) {
                 return 0;
@@ -130,9 +140,11 @@ class P84LargestRectangleInHistogram {
             len += 2;
             heights = newHeights;
             int area = 0;
+            //java 推荐stack 使用 ArrayDeque
             Deque<Integer> stack = new ArrayDeque<>();
             stack.addLast(0);
             for (int i = 1; i < len; i++) {
+                //栈顶元素高度大于新栈顶元素， 可计算出面积
                 while (heights[stack.peekLast()] > heights[i]) {
                     int height = heights[stack.removeLast()];
                     int width = i - stack.peekLast() - 1;
