52226295835e82fd45e61263459e917387210ae8
jiaxianyang <jiaxianyang@jd.com>
2021-09-01 20:10:43 +0800

    [242]有效的字母异位词

--- src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[242]有效的字母异位词.java
+++ src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[242]有效的字母异位词.java
@@ -33,6 +33,9 @@
 
 package com.rj.leetcode_solution.leetcode.editor.cn;
 
+import java.util.HashMap;
+import java.util.Map;
+
 //java:有效的字母异位词
 class P242ValidAnagram{
     public static void main(String[] args){
@@ -54,17 +57,51 @@ class Solution {
         //2.哈希表  长度相同，
         // 1. 如果 t 比 s 字母多，那么会出现负数
         // 2. 如果 t 比 s 字段少， 那么长度一定会不一样
+//        if (s.length() != t.length()) {
+//            return false;
+//        }
+//        int[] table = new int[26];
+//        for (int i = 0; i < s.length(); i++) {
+//            table[s.charAt(i) - 'a']++;
+//        }
+//
+//        for (int i = 0; i < t.length(); i++) {
+//            table[t.charAt(i) - 'a']--;
+//            if (table[t.charAt(i) - 'a'] < 0) {
+//                return false;
+//            }
+//        }
+//        return true;
+
+        //3. 哈希表， 个人认为比较好的解法
+//        if (s.length() != t.length()) {
+//            return false;
+//        }
+//        int[] arr = new int[26];
+//        for (int i = 0; i < s.length(); i++) {
+//            arr[s.charAt(i) - 'a']++;
+//            arr[t.charAt(i) - 'a']--;
+//        }
+//        for (int i = 0; i < 26; i++) {
+//            if (arr[i] != 0) {
+//                return false;
+//            }
+//        }
+//        return true;
+
+        //4. 哈希表
         if (s.length() != t.length()) {
             return false;
         }
-        int[] table = new int[26];
+        Map<Character, Integer> table = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
-            table[s.charAt(i) - 'a']++;
+            char ch = s.charAt(i);
+            table.put(ch, table.getOrDefault(ch, 0) + 1);
         }
-
         for (int i = 0; i < t.length(); i++) {
-            table[t.charAt(i) - 'a']--;
-            if (table[t.charAt(i) - 'a'] < 0) {
+            char ch = t.charAt(i);
+            table.put(ch, table.getOrDefault(ch, 0) - 1);
+            if (table.get(ch) < 0) {
                 return false;
             }
         }
