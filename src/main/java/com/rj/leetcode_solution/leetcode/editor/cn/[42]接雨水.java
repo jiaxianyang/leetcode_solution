f4dc1dca3c9b8c87f70081b9ff07b6bff516fbf6
jiaxianyang <jiaxianyang@jd.com>
2021-09-05 10:28:10 +0800

    [42]接雨水暴力、动态规划

--- src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[42]接雨水.java
+++ src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[42]接雨水.java
@@ -32,76 +32,78 @@
 
 package com.rj.leetcode_solution.leetcode.editor.cn;
 
-import java.util.Deque;
-import java.util.LinkedList;
-
 //java:接雨水
-class P42TrappingRainWater{
-    public static void main(String[] args){
+class P42TrappingRainWater {
+    public static void main(String[] args) {
         Solution solution = new P42TrappingRainWater().new Solution();
-        int[] height = new int[]{2, 4, 0, 3, 2, 5};
+        int[] height = new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
         int trap = solution.trap(height);
-        System.out.println();
+        System.out.println(trap);
     }
+
     //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int trap(int[] height) {
-        //1. 暴力法，当前坐标的装水量，左边界最大值，右边界做大值中 最小值，来确定所能装的谁
-//        int ans = 0;
-//        int size = height.length;
-//        for (int i = 1; i < size - 1; i++) {
-//            int max_left = 0, max_right = 0;
-//            for (int j = i; j >= 0; j--) {
-//                max_left = Math.max(max_left, height[j]);
-//            }
-//            for (int j = i; j < size; j++) {
-//                max_right = Math.max(max_right, height[j]);
-//            }
-//            ans += Math.min(max_left, max_right) - height[i];
-//        }
-//        return ans;
-
-        //2. 动态规划 暴力法，每次都要从新计算左面和右面最高的柱子，是否可以计算出来
-//        if (height == null || height.length == 0) {
-//            return 0;
-//        }
-//        int ans = 0;
-//        int size = height.length;
-//        int[] leftMaxArr = new int[size];
-//        int[] rightMaxArr = new int[size];
-//        leftMaxArr[0] = height[0];
-//        for (int i = 1; i < size; i++) {
-//            leftMaxArr[i] = Math.max(height[i], leftMaxArr[i - 1]);
-//        }
-//        rightMaxArr[size - 1] = height[size - 1];
-//        for (int i = size - 2; i >= 0; i--) {
-//            rightMaxArr[i] = Math.max(height[i], rightMaxArr[i + 1]);
-//        }
-//        for (int i = 1; i < size - 1; i++) {
-//            ans += Math.min(leftMaxArr[i], rightMaxArr[i]) - height[i];
-//        }
-//        return ans;
-
-        //3.栈应用
-        int ans = 0;
-        Deque<Integer> stack = new LinkedList<>();
-        int n = height.length;
-        for (int i = 0; i < n; ++i) {
-            while (!stack.isEmpty() && height[i] > height[stack.peek()]) {
-                int top = stack.pop();
-                if (stack.isEmpty()) {
-                    break;
-                }
-                int left = stack.peek();
-                int currWidth = i - left - 1;
-                int currHeight = Math.min(height[left], height[i]) - height[top];
-                ans += currWidth * currHeight;
-            }
-            stack.push(i);
+    class Solution {
+        public int trap(int[] height) {
+            //1. 暴力法，当前坐标的装水量获取，左边界最大值，右边界最大值 取俩边最小值，来确定当前柱子能装的水量
+//            return trapMethod1(height);
+            //2. 动态规划：暴力法，每次都要从新计算左边界、右边界的最大值，存在重复计算，我们是否可以缓存起来
+            return trapMethod2(height);
+            //3.栈应用
         }
-        return ans;
+
+        /**
+         * 1. 暴力法，当前坐标的装水量获取，左边界最大值，右边界最大值 取俩边最小值，来确定当前柱子能装的水量
+         */
+        private int trapMethod1(int[] height) {
+            int ans = 0;
+            int size = height.length;
+            for (int i = 1; i < size - 1; i++) {
+                int maxLeft = 0;
+                int maxRight = 0;
+                //获取左边的最大值, 从i开始，如果左边没有大于当前柱子，那么计算的装水体积为0
+                for (int j = i; j >= 0; j--) {
+                    maxLeft = Math.max(maxLeft, height[j]);
+                }
+                //获取右边的最大值, 从i开始, 如果右边没有柱子高度没有大于当前柱子的，那么计算出的装水体积为0
+                for (int j = i; j < size; j++) {
+                    maxRight = Math.max(maxRight, height[j]);
+                }
+                ans += Math.min(maxLeft, maxRight) - height[i];
+            }
+            return ans;
+        }
+
+        /**
+         * 2. 动态规划：暴力法，每次都要从新计算左边界、右边界的最大值，存在重复计算，我们是否可以缓存起来
+         */
+        private int trapMethod2(int[] height) {
+            if (height == null || height.length == 0) {
+                return 0;
+            }
+            int ans = 0;
+            int length = height.length;
+            //存储左边界最大高度数组
+            int[] leftMaxArr = new int[length];
+            //存储右边界最大高度数据
+            int[] rightMaxArr = new int[length];
+            //左边界的最大值，当前柱子高度与左边--,位置的高度进行比较，计算每一个位置的最大柱子高度
+            leftMaxArr[0] = height[0];
+            for (int i = 1; i < length; i++) {
+                leftMaxArr[i] = Math.max(height[i], leftMaxArr[i - 1]);
+            }
+            //右边界的最大值，当前柱子高度与右边++,位置的高度进行比较，计算每一个位置的最大柱子高度
+            rightMaxArr[length - 1] = height[length - 1];
+            for (int i = length - 2; i >= 0; i--) {
+                rightMaxArr[i] = Math.max(height[i], rightMaxArr[i + 1]);
+            }
+            //利用缓存后的左右边界的最大高度，可以计算每一个位置的存储水的体积，进而计算出总的装水的体积
+            for (int i = 0; i < length; i++) {
+                ans += Math.min(leftMaxArr[i], rightMaxArr[i]) - height[i];
+            }
+            return ans;
+        }
+
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
