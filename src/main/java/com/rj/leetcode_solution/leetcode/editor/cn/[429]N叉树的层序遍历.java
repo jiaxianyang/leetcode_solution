e3b65f352ae8794ab6a8dd6dd42fa6e352538e93
jiaxianyang <jiaxianyang@jd.com>
2021-09-22 07:56:17 +0800

    [429]N叉树的层序遍历 广度优先、递归解法

--- src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[429]N叉树的层序遍历.java
+++ src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[429]N叉树的层序遍历.java
@@ -36,13 +36,14 @@
 package com.rj.leetcode_solution.leetcode.editor.cn;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
 //java:N 叉树的层序遍历
-class P429NAryTreeLevelOrderTraversal{
-    public static void main(String[] args){
+class P429NAryTreeLevelOrderTraversal {
+    public static void main(String[] args) {
         Solution solution = new P429NAryTreeLevelOrderTraversal().new Solution();
     }
     //leetcode submit region begin(Prohibit modification and deletion)
@@ -65,45 +66,109 @@ class Node {
 };
 */
 
-class Solution {
-    public List<List<Integer>> levelOrder(Node root) {
-        //方法1. 利用队列实现广度优先遍历
-        return levelOrderBfs(root);
-    }
+    class Solution {
 
-    private List<List<Integer>> levelOrderBfs(Node root) {
-        List<List<Integer>> result = new ArrayList<>();
-        if (root == null) {
+        public List<List<Integer>> levelOrder(Node root) {
+            //方法1. 利用队列实现广度优先遍历
+//            return levelOrderBfs(root);
+            //方法2. 简化版的广度优先搜索
+//            return levelOrderSampleBfs(root);
+            //方法3. 递归
+            return recursionLevelOrder(root);
+        }
+
+        /**
+         * 我们可以使用递归来解决这个问题，通常我们不能使用递归进行广度优先搜索。这是因为广度优先搜索基于
+         * 队列，而递归运行时使用的堆栈，适合深度优先遍历。
+         * <p>
+         * 但是在本题中，只要我们知道节点在哪一层并确保在那一层的列表顺序正确就可以了
+         *
+         * @param root
+         * @return
+         */
+        private List<List<Integer>> recursionResult = new ArrayList<>();
+
+        private List<List<Integer>> recursionLevelOrder(Node root) {
+            if (root != null) {
+                traverseNode(root, 0);
+            }
+            return recursionResult;
+        }
+
+        private void traverseNode(Node node, int level) {
+            if (recursionResult.size() <= level) {
+                recursionResult.add(new ArrayList<>());
+            }
+            recursionResult.get(level).add(node.val);
+            for (Node child : node.children) {
+                traverseNode(child, level + 1);
+            }
+        }
+
+        /**
+         * 简单的BFS(广度优先遍历)
+         *
+         * @param root 树根节点
+         * @return List<List < Integer>>
+         */
+        private List<List<Integer>> levelOrderSampleBfs(Node root) {
+            List<List<Integer>> result = new ArrayList<>();
+            if (root == null) {
+                return result;
+            }
+            List<Node> previousLayer = Arrays.asList(root);
+            while (!previousLayer.isEmpty()) {
+                List<Node> currentLayer = new ArrayList<>();
+                List<Integer> previousVals = new ArrayList<>();
+                for (Node node : previousLayer) {
+                    previousVals.add(node.val);
+                    currentLayer.addAll(node.children);
+                }
+                result.add(previousVals);
+                previousLayer = currentLayer;
+            }
             return result;
         }
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(root);
-        while (!queue.isEmpty()) {
-            List<Integer> levelList = new ArrayList<>();
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                Node node = queue.poll();
-                levelList.add(node.val);
-                queue.addAll(node.children);
-            }
-            result.add(levelList);
-        }
-        return result;
-    }
 
-    private void bfsTemplate(Node root) {
-        List<Integer> values = new ArrayList<>();
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(root);
-        while (!queue.isEmpty()) {
-            Node nextNode = queue.remove();
-            values.add(nextNode.val);
-            for (Node child : nextNode.children) {
-                queue.add(child);
+        /**
+         * 利用队列实现广度优先遍历
+         *
+         * @param root 树根节点
+         * @return List<List < Integer>>
+         */
+        private List<List<Integer>> levelOrderBfs(Node root) {
+            List<List<Integer>> result = new ArrayList<>();
+            if (root == null) {
+                return result;
+            }
+            Queue<Node> queue = new LinkedList<>();
+            queue.add(root);
+            while (!queue.isEmpty()) {
+                List<Integer> levelList = new ArrayList<>();
+                int size = queue.size();
+                for (int i = 0; i < size; i++) {
+                    Node node = queue.poll();
+                    levelList.add(node.val);
+                    queue.addAll(node.children);
+                }
+                result.add(levelList);
+            }
+            return result;
+        }
+
+        private void bfsTemplate(Node root) {
+            List<Integer> values = new ArrayList<>();
+            Queue<Node> queue = new LinkedList<>();
+            queue.add(root);
+            while (!queue.isEmpty()) {
+                Node nextNode = queue.remove();
+                values.add(nextNode.val);
+                for (Node child : nextNode.children) {
+                    queue.add(child);
+                }
             }
         }
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
