34081f446893bc61b1c76cf243a4d92a4aa1a38b
jiaxianyang <jiaxianyang@jd.com>
2021-10-02 13:04:49 +0800

    [22] 括号生成 暴力、递归、回溯法

--- src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[22]括号生成.java
+++ src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[22]括号生成.java
@@ -42,12 +42,34 @@ class P22GenerateParentheses {
     class Solution {
         public List<String> generateParenthesis(int n) {
             //1. 暴力法
-            return generateParenthesisMethod(n);
+//            return generateParenthesisMethod(n);
 
             //2.递归
 //            List<String> result = new ArrayList<>();
 //            generate(0, 0, n, "", result);
 //            return result;
+
+            //3.回溯法
+            List<String> ans = new ArrayList<>();
+            backtrack(ans, new StringBuilder(), 0, 0, n);
+            return ans;
+        }
+
+        private void backtrack(List<String> ans, StringBuilder cur, int open, int close, int max) {
+            if (cur.length() == max * 2) {
+                ans.add(cur.toString());
+                return;
+            }
+            if (open < max) {
+                cur.append("(");
+                backtrack(ans, cur, open + 1, close, max);
+                cur.deleteCharAt(cur.length() - 1);
+            }
+            if (close < open) {
+                cur.append(")");
+                backtrack(ans, cur, open, close + 1, max);
+                cur.deleteCharAt(cur.length() - 1);
+            }
         }
 
         /**
