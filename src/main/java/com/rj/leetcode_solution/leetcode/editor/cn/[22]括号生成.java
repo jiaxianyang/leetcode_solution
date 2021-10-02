00eabbfb352ce18d891770730a3bb981326ce544
jiaxianyang <jiaxianyang@jd.com>
2021-10-02 09:58:24 +0800

    [22] 括号生成 暴力、递归

--- src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[22]括号生成.java
+++ src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[22]括号生成.java
@@ -50,20 +50,32 @@ class P22GenerateParentheses {
             return result;
         }
 
-        private void generate(int left, int right, int n, String s, List<String> result) {
+        /**
+         * 解题思路：我们想象成有很多格子，我们往里面放入左右括号，如果想要最终满足结果，那么要同事符合以下规则：
+         * 1. 我们在组装结果的时候，放入的右边的括号数量不能大于左边，如果大于那么就会组装成无用的结果 所以（right < left）
+         * 2. 我们放入的左括号数量不能大于n,如果大于n，最终组装的结果也是不满足的
+         * 3. 当左括号数量和右括号数量相当并且都等于n的时候，为我们想要的结果
+         *
+         * @param left   左括号数量
+         * @param right  有括号数量
+         * @param max    n
+         * @param str    字符串
+         * @param result 结果集
+         */
+        private void generate(int left, int right, int max, String str, List<String> result) {
             //terminator
-            if (left == n && right == n) {
-                result.add(s);
+            if (left == max && right == max) {
+                result.add(str);
                 return;
             }
             //process
 
             //drill down
-            if (left < n) {
-                generate(left + 1, right, n, s + "(", result);
+            if (left < max) {
+                generate(left + 1, right, max, str + "(", result);
             }
             if (right < left) {
-                generate(left, right + 1, n, s + ")", result);
+                generate(left, right + 1, max, str + ")", result);
             }
             //reverse status
         }
