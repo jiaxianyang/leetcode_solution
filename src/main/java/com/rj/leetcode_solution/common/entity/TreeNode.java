b434656e420fb3b8351e701e30af86de76a4f76e
jiaxianyang <jiaxianyang@jd.com>
2021-10-26 16:57:22 +0800

    [226] 翻转二叉树

--- src/main/java/com/rj/leetcode_solution/common/entity/TreeNode.java
+++ src/main/java/com/rj/leetcode_solution/common/entity/TreeNode.java
@@ -1,5 +1,12 @@
 package com.rj.leetcode_solution.common.entity;
 
+/**
+ * TreeNode简介
+ *
+ * 二叉树公用实体
+ * @author jiaxianyang
+ * @date 2021-10-26 14:51
+ */
 public class TreeNode {
     public int val;
     public TreeNode left;
