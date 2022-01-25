55470d35e419ea4b8857b93350a2b746e96db3bd
jiaxianyang <jiaxianyang@jd.com>
2022-01-25 21:09:46 +0800

    feature: [297]二叉树的序列号与反序列化

---
+++ src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[297]二叉树的序列化与反序列化.java
@@ -0,0 +1,116 @@
+//序列化是将一个数据结构或者对象转换为连续的比特位的操作，进而可以将转换后的数据存储在一个文件或者内存中，同时也可以通过网络传输到另一个计算机环境，采取相反方
+//式重构得到原数据。 
+//
+// 请设计一个算法来实现二叉树的序列化与反序列化。这里不限定你的序列 / 反序列化算法执行逻辑，你只需要保证一个二叉树可以被序列化为一个字符串并且将这个字符串
+//反序列化为原始的树结构。 
+//
+// 提示: 输入输出格式与 LeetCode 目前使用的方式一致，详情请参阅 LeetCode 序列化二叉树的格式。你并非必须采取这种方式，你也可以采用其他的
+//方法解决这个问题。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [1,2,3,null,null,4,5]
+//输出：[1,2,3,null,null,4,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = []
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = [1]
+//输出：[1]
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：root = [1,2]
+//输出：[1,2]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中结点数在范围 [0, 10⁴] 内 
+// -1000 <= Node.val <= 1000 
+// 
+// Related Topics 树 深度优先搜索 广度优先搜索 设计 字符串 二叉树 👍 696 👎 0
+
+package com.rj.leetcode_solution.leetcode.editor.cn;
+
+import com.rj.leetcode_solution.common.entity.TreeNode;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+//java:二叉树的序列化与反序列化
+class P297SerializeAndDeserializeBinaryTree{
+    public static void main(String[] args){
+        Codec solution = new P297SerializeAndDeserializeBinaryTree().new Codec();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+    public String serialize(TreeNode root) {
+        return rserialize(root, "");
+    }
+
+    public TreeNode deserialize(String data) {
+        String[] dataArray = data.split(",");
+        List<String> dataList = new LinkedList<>(Arrays.asList(dataArray));
+        return rdeserialize(dataList);
+    }
+
+    public String rserialize(TreeNode root, String str) {
+        if (root == null) {
+            str += "None,";
+        } else {
+            str += root.val + ",";
+            str = rserialize(root.left, str);
+            str = rserialize(root.right, str);
+        }
+        return str;
+    }
+
+    public TreeNode rdeserialize(List<String> dataList) {
+        if (dataList.get(0).equals("None")) {
+            dataList.remove(0);
+            return null;
+        }
+
+        TreeNode root = new TreeNode(Integer.parseInt(dataList.get(0)));
+        dataList.remove(0);
+        root.left = rdeserialize(dataList);
+        root.right = rdeserialize(dataList);
+
+        return root;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// TreeNode ans = deser.deserialize(ser.serialize(root));
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
