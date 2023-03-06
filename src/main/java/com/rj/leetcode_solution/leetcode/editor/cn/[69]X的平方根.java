de6ffca5de845e97c2b6d73f16c6a2c851613e37
jiaxianyang <jiaxianyang@jd.com>
2023-03-06 10:51:44 +0800

    feature: [69] 二分查找平方根

---
+++ src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[69]X的平方根.java
@@ -0,0 +1,65 @@
+//给你一个非负整数 x ，计算并返回 x 的 算术平方根 。 
+//
+// 由于返回类型是整数，结果只保留 整数部分 ，小数部分将被 舍去 。 
+//
+// 注意：不允许使用任何内置指数函数和算符，例如 pow(x, 0.5) 或者 x ** 0.5 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：x = 4
+//输出：2
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：x = 8
+//输出：2
+//解释：8 的算术平方根是 2.82842..., 由于返回类型是整数，小数部分将被舍去。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= x <= 2³¹ - 1 
+// 
+//
+// Related Topics 数学 二分查找 👍 1274 👎 0
+
+package com.rj.leetcode_solution.leetcode.editor.cn;
+
+//java:x 的平方根
+class P69Sqrtx {
+    public static void main(String[] args) {
+        Solution solution = new P69Sqrtx().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int mySqrt(int x) {
+            if (x == 0 || x == 1) {
+                return x;
+            }
+            int left = 0;
+            int right = x;
+            int ans = -1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if ((long) mid * mid > x) {
+                    right = mid - 1;
+                } else {
+                    ans = mid;
+                    left = mid + 1;
+                }
+            }
+            return ans;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
