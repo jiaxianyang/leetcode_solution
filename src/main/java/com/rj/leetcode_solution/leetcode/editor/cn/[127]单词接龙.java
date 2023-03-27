97aa5d09de65582abbd0713aa26bb793793b1c5f
jiaxianyang <jiaxianyang@jd.com>
2023-03-27 14:01:35 +0800

    feature: [127] 单词接龙-

--- src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[127]单词接龙.java
+++ src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[127]单词接龙.java
@@ -68,9 +68,12 @@ class P127WordLadder {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        Map<String, Integer> wordId = new HashMap<>();
-        List<List<Integer>> edge = new ArrayList<>();
+
+        //双向广度优先搜索
+        Map<String, Integer> wordId = new HashMap<String, Integer>();
+        List<List<Integer>> edge = new ArrayList<List<Integer>>();
         int nodeNum = 0;
+
         public int ladderLength(String beginWord, String endWord, List<String> wordList) {
             for (String word : wordList) {
                 addEdge(word);
@@ -79,33 +82,59 @@ class P127WordLadder {
             if (!wordId.containsKey(endWord)) {
                 return 0;
             }
-            int[] dis = new int[nodeNum];
-            Arrays.fill(dis, Integer.MAX_VALUE);
-            int beginId = wordId.get(beginWord), endId = wordId.get(endWord);
-            dis[beginId] = 0;
-            Queue<Integer> que = new LinkedList<>();
-            que.offer(beginId);
-            while (!que.isEmpty()) {
-                int x = que.poll();
-                if (x == endId) {
-                    return dis[endId] / 2 + 1;
+
+            int[] disBegin = new int[nodeNum];
+            Arrays.fill(disBegin, Integer.MAX_VALUE);
+            int beginId = wordId.get(beginWord);
+            disBegin[beginId] = 0;
+            Queue<Integer> queBegin = new LinkedList<Integer>();
+            queBegin.offer(beginId);
+
+            int[] disEnd = new int[nodeNum];
+            Arrays.fill(disEnd, Integer.MAX_VALUE);
+            int endId = wordId.get(endWord);
+            disEnd[endId] = 0;
+            Queue<Integer> queEnd = new LinkedList<Integer>();
+            queEnd.offer(endId);
+
+            while (!queBegin.isEmpty() && !queEnd.isEmpty()) {
+                int queBeginSize = queBegin.size();
+                for (int i = 0; i < queBeginSize; ++i) {
+                    int nodeBegin = queBegin.poll();
+                    if (disEnd[nodeBegin] != Integer.MAX_VALUE) {
+                        return (disBegin[nodeBegin] + disEnd[nodeBegin]) / 2 + 1;
+                    }
+                    for (int it : edge.get(nodeBegin)) {
+                        if (disBegin[it] == Integer.MAX_VALUE) {
+                            disBegin[it] = disBegin[nodeBegin] + 1;
+                            queBegin.offer(it);
+                        }
+                    }
                 }
-                for (Integer it : edge.get(x)) {
-                    if (dis[it] == Integer.MAX_VALUE) {
-                        dis[it] = dis[x] + 1;
-                        que.offer(it);
+
+                int queEndSize = queEnd.size();
+                for (int i = 0; i < queEndSize; ++i) {
+                    int nodeEnd = queEnd.poll();
+                    if (disBegin[nodeEnd] != Integer.MAX_VALUE) {
+                        return (disBegin[nodeEnd] + disEnd[nodeEnd]) / 2 + 1;
+                    }
+                    for (int it : edge.get(nodeEnd)) {
+                        if (disEnd[it] == Integer.MAX_VALUE) {
+                            disEnd[it] = disEnd[nodeEnd] + 1;
+                            queEnd.offer(it);
+                        }
                     }
                 }
             }
             return 0;
         }
 
-        private void addEdge(String word) {
+        public void addEdge(String word) {
             addWord(word);
-            Integer id1 = wordId.get(word);
+            int id1 = wordId.get(word);
             char[] array = word.toCharArray();
             int length = array.length;
-            for (int i = 0; i < length; i++) {
+            for (int i = 0; i < length; ++i) {
                 char tmp = array[i];
                 array[i] = '*';
                 String newWord = new String(array);
@@ -117,12 +146,70 @@ class P127WordLadder {
             }
         }
 
-        private void addWord(String word) {
+        public void addWord(String word) {
             if (!wordId.containsKey(word)) {
                 wordId.put(word, nodeNum++);
-                edge.add(new ArrayList<>());
+                edge.add(new ArrayList<Integer>());
             }
         }
+
+
+    //1、广度优先搜索 + 优化建图
+//      Map<String, Integer> wordId = new HashMap<>();
+//        List<List<Integer>> edge = new ArrayList<>();
+//        int nodeNum = 0;
+//        public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+//            for (String word : wordList) {
+//                addEdge(word);
+//            }
+//            addEdge(beginWord);
+//            if (!wordId.containsKey(endWord)) {
+//                return 0;
+//            }
+//            int[] dis = new int[nodeNum];
+//            Arrays.fill(dis, Integer.MAX_VALUE);
+//            int beginId = wordId.get(beginWord), endId = wordId.get(endWord);
+//            dis[beginId] = 0;
+//            Queue<Integer> que = new LinkedList<>();
+//            que.offer(beginId);
+//            while (!que.isEmpty()) {
+//                int x = que.poll();
+//                if (x == endId) {
+//                    return dis[endId] / 2 + 1;
+//                }
+//                for (Integer it : edge.get(x)) {
+//                    if (dis[it] == Integer.MAX_VALUE) {
+//                        dis[it] = dis[x] + 1;
+//                        que.offer(it);
+//                    }
+//                }
+//            }
+//            return 0;
+//        }
+//
+//        private void addEdge(String word) {
+//            addWord(word);
+//            Integer id1 = wordId.get(word);
+//            char[] array = word.toCharArray();
+//            int length = array.length;
+//            for (int i = 0; i < length; i++) {
+//                char tmp = array[i];
+//                array[i] = '*';
+//                String newWord = new String(array);
+//                addWord(newWord);
+//                int id2 = wordId.get(newWord);
+//                edge.get(id1).add(id2);
+//                edge.get(id2).add(id1);
+//                array[i] = tmp;
+//            }
+//        }
+//
+//        private void addWord(String word) {
+//            if (!wordId.containsKey(word)) {
+//                wordId.put(word, nodeNum++);
+//                edge.add(new ArrayList<>());
+//            }
+//        }
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
