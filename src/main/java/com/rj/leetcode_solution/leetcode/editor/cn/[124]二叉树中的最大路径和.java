836f94a9e2f3432130030adc34a08ed8d48f4c31
jiaxianyang <jiaxianyang@jd.com>
2024-01-23 11:28:03 +0800

    feature: [124] 解法,增加注释

--- src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[124]二叉树中的最大路径和.java
+++ src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[124]二叉树中的最大路径和.java
@@ -72,10 +72,14 @@ class Solution {
         if (node == null) {
             return 0;
         }
+        // 递归计算左右子节点的最大贡献值
+        // 只有在最大贡献值大于 0 时，才会选取对应子节点
         int leftGain = Math.max(maxGain(node.left), 0);
         int rightGain = Math.max(maxGain(node.right), 0);
+        // 节点的最大路径和取决于该节点的值与该节点的左右子节点的最大贡献值
         int nowPathMaxValue = leftGain + rightGain + node.val;
         maxSum = Math.max(maxSum, nowPathMaxValue);
+        // 返回节点的最大贡献值
         return node.val + Math.max(leftGain, rightGain);
     }
 
