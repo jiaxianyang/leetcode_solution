68413fa1727879dd955ab2c6d5fd36ad38f1946b
jiaxianyang <jiaxianyang@jd.com>
2024-03-18 19:55:56 +0800

    feat: 【28】kmp 字符串问题

--- src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[28]找出字符串中第一个匹配项的下标.java
+++ src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[28]找出字符串中第一个匹配项的下标.java
@@ -40,50 +40,43 @@ class P28FindTheIndexOfTheFirstOccurrenceInAString{
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int strStr(String haystack, String needle) {
-        if (needle.isEmpty()) {
+        if (needle.length() == 0) {
             return 0;
         }
-        //分别读取原串和匹配串的长度
-        int n = haystack.length(), m = needle.length();
-        //原串和匹配串前面都加空格，使其下标从1开始
-        haystack = " " + haystack;
-        needle = " " + needle;
-        char[] s = haystack.toCharArray();
-        char[] p = needle.toCharArray();
-        //构建next数组，数组长度为匹配串的长度（next数组是和匹配串相关的）
-        int[] next = new int[m + 1];
-        //构建过程i = 2,j = 0 开始，i小于匹配串长度 【构造i 从 2 开始】
-        for (int i = 2, j = 0; i <= m; i++) {
-            //匹配成功的话，先让j++
-            while (j > 0 && p[i] != p[j + 1]) {
-                j = next[j];
+        //初始化
+        int[] next = new int[needle.length()];
+        getNext(next, needle);
+        int j = 0;
+        for (int i = 0; i < haystack.length(); i++) {
+            while (j > 0 && needle.charAt(j) != haystack.charAt(i)) {
+                j = next[j - 1];
             }
-            //如果匹配成功的话先让j++
-            if (p[i] == p[j + 1]) {
-                j ++;
-            }
-            //更新next[i],结束本次循环， i++
-            next[i] = j;
-        }
-
-        //匹配过程， i =  1, j = 0开始， i 小于等于原串长度【匹配i 从 1开始】
-        for (int i = 1, j = 0; i <= n; i++) {
-            //匹配不成功 j = next[j]
-            while (j > 0 && s[i] != p[j + 1]) {
-                j = next[j];
-            }
-            //如果匹配成功的话先让 j++ ，结束本次循环后 i++
-            if (s[i] == p[j + 1]) {
+            if (needle.charAt(j) == haystack.charAt(i)) {
                 j++;
             }
-            //整一段匹配成功，直接返回下标
-            if (j == m) {
-                return i - m;
+            if (j == needle.length()) {
+                return i - needle.length() + 1;
             }
         }
-        return -1;
+        return  - 1;
+
     }
-}
+
+        private void getNext(int[] next, String needle) {
+            int j = 0;
+            next[0] = 0;
+            for (int i = 1; i < needle.length(); i++) {
+                while (j > 0 && needle.charAt(j) != needle.charAt(i)) {
+                    j = next[j - 1];
+                }
+                if (needle.charAt(j) == needle.charAt(i)) {
+                    j++;
+                }
+                next[i] = j;
+            }
+
+        }
+    }
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
