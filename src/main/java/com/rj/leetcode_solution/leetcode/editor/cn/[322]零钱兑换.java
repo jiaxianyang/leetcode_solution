7a997d3e23af8e8295ea543bbd8f689ed2c9ec56
jiaxianyang <jiaxianyang@jd.com>
2024-03-25 14:22:53 +0800

    feat: dp【322】零钱兑换问题

--- src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[322]零钱兑换.java
+++ src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[322]零钱兑换.java
@@ -40,42 +40,38 @@
 
 package com.rj.leetcode_solution.leetcode.editor.cn;
 
+import java.util.Arrays;
+
 //java:零钱兑换
 class P322CoinChange {
     public static void main(String[] args) {
         Solution solution = new P322CoinChange().new Solution();
+
+        int minCoin = solution.coinChange(new int[]{1, 2, 5}, 11);
+        System.out.println(minCoin);
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        public int coinChange(int[] coins, int amount) {
-            if (amount < 1) {
-                return 0;
-            }
-            return coinChange(coins, amount, new int[amount]);
-        }
 
-        private int coinChange(int[] coins, int rem, int[] count) {
-            //terminator
-            if (rem < 0) {
-                return -1;
-            }
-            if (rem == 0) {
-                return 0;
-            }
-            if (count[rem - 1] != 0) {
-                return count[rem - 1];
-            }
-            int min = Integer.MAX_VALUE;
-            for (int coin : coins) {
-                int res = coinChange(coins, rem - coin, count);
-                if (res >= 0 && res < min) {
-                    min = 1 + res;
+//        f(n)= min{f(n-k),for kin[1，2，5]})+ 1
+        public int coinChange(int[] coins, int amount) {
+            //动态规划类似爬楼梯  f(n)= min{f(n-k),for kin[1，2，5]})+ 1
+            //当前值 肯定是从 减去 conins 中 任意值 得来的,把当前硬币加进来 再 + 1
+            int max = amount + 1;
+            int[] dp = new int[amount + 1];
+            Arrays.fill(dp, max);
+            dp[0] = 0;
+            for (int i = 1; i <= amount; i++) {
+                for (int j = 0; j < coins.length; j++) {
+                    if (coins[j] <= i) {
+                        dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                    }
                 }
             }
-            count[rem - 1] = (min == Integer.MAX_VALUE) ? -1 : min;
-            return count[rem - 1];
+            return dp[amount] > amount ? -1 : dp[amount];
         }
+
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
