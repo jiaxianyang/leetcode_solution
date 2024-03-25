691dc036dcc2f610f350dbc635fa47b73006d167
jiaxianyang <jiaxianyang@jd.com>
2024-03-25 14:30:03 +0800

    feat: 【322】零钱兑换问题

--- src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[322]零钱兑换.java
+++ src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[322]零钱兑换.java
@@ -72,6 +72,34 @@ class P322CoinChange {
             return dp[amount] > amount ? -1 : dp[amount];
         }
 
+//        public int coinChange(int[] coins, int amount) {
+//            if (amount < 1) {
+//                return 0;
+//            }
+//            return coinChange(coins, amount, new int[amount]);
+//        }
+//
+//        private int coinChange(int[] coins, int rem, int[] count) {
+//            if (rem < 0) {
+//                return -1;
+//            }
+//            if (rem == 0) {
+//                return 0;
+//            }
+//            if (count[rem - 1] != 0) {
+//                return count[rem - 1];
+//            }
+//            int min = Integer.MAX_VALUE;
+//            for (int coin : coins) {
+//                int res = coinChange(coins, rem - coin, count);
+//                if (res >= 0 && res < min) {
+//                    min = 1 + res;
+//                }
+//            }
+//            count[rem - 1] = (min == Integer.MAX_VALUE) ? -1 : min;
+//            return count[rem - 1];
+//        }
+
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
