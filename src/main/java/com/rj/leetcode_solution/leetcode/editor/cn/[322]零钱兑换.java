1f150756f5fe10cf5834a12f6394c18522c10ff3
jiaxianyang <jiaxianyang@jd.com>
2024-03-26 10:50:35 +0800

    feat: 【322】零钱对话

--- src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[322]零钱兑换.java
+++ src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[322]零钱兑换.java
@@ -58,18 +58,31 @@ class P322CoinChange {
         public int coinChange(int[] coins, int amount) {
             //动态规划类似爬楼梯  f(n)= min{f(n-k),for kin[1，2，5]})+ 1
             //当前值 肯定是从 减去 conins 中 任意值 得来的,把当前硬币加进来 再 + 1
-            int max = amount + 1;
+//            int max = amount + 1;
+//            int[] dp = new int[amount + 1];
+//            Arrays.fill(dp, max);
+//            dp[0] = 0;
+//            for (int i = 1; i <= amount; i++) {
+//                for (int j = 0; j < coins.length; j++) {
+//                    if (coins[j] <= i) {
+//                        dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+//                    }
+//                }
+//            }
+//            return dp[amount] > amount ? -1 : dp[amount];
+
             int[] dp = new int[amount + 1];
-            Arrays.fill(dp, max);
-            dp[0] = 0;
             for (int i = 1; i <= amount; i++) {
-                for (int j = 0; j < coins.length; j++) {
-                    if (coins[j] <= i) {
-                        dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
-                    }
+                dp[i] = -1;
+            }
+            for (int i = 1; i <= amount; i++) {
+                for (int coin : coins) {
+                    if (i < coin) continue;
+                    if (dp[i - coin] == - 1) continue;
+                    if (dp[i] == - 1 || dp[i] > dp[i - coin] + 1) dp[i] = dp[i - coin] + 1;
                 }
             }
-            return dp[amount] > amount ? -1 : dp[amount];
+            return dp[amount];
         }
 
 //        public int coinChange(int[] coins, int amount) {
