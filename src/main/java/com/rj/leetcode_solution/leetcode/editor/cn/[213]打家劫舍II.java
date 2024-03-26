08c129bb6d24627bc8c2587d666282dbf7be94c6
jiaxianyang <jiaxianyang@jd.com>
2024-03-26 20:22:35 +0800

    feat: 【213】打家劫舍

--- src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[213]打家劫舍II.java
+++ src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[213]打家劫舍II.java
@@ -60,7 +60,7 @@ class Solution {
 
         private int robRange(int[] nums, int start, int end) {
             int len = end - start  + 1;
-            int[] dp = new int[len + 1];
+            int[] dp = new int[len];
             dp[0] = nums[start];
             dp[1] = Math.max(nums[start], nums[start + 1]);
             for (int i = 2; i < len; i++) {
@@ -68,6 +68,15 @@ class Solution {
             }
             return dp[len - 1];
         }
+
+
+        //        int[] dp = new int[length];
+//        dp[0] = nums[0];
+//        dp[1] = Math.max(nums[0], nums[1]);
+//        for (int i = 2; i < length; i++) {
+//            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+//        }
+//        return dp[length - 1];
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
