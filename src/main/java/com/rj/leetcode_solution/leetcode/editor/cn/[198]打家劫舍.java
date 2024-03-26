8b92a581d30ce15ce4f2ff2484d9db8efd94ca42
jiaxianyang <jiaxianyang@jd.com>
2024-03-26 11:26:59 +0800

    feat: 【198】打家劫舍

--- src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[198]打家劫舍.java
+++ src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[198]打家劫舍.java
@@ -42,6 +42,21 @@ class P198HouseRobber{
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int rob(int[] nums) {
+        //step 1
+//        if (nums == null || nums.length == 0) {
+//            return 0;
+//        }
+//        int n = nums.length;
+//        int[][] dp = new int[n][2];
+//        dp[0][0] = 0;
+//        dp[0][1] = nums[0];
+//        for (int i = 1; i < n; i++) {
+//            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]);
+//            dp[i - 1][1] = dp[i - 1][0] + nums[i];
+//        }
+//        return Math.max(dp[n - 1][0], dp[n - 1][1]);
+
+        //step 2
 
         if (nums == null || nums.length == 0) {
             return 0;
@@ -57,6 +72,20 @@ class Solution {
             dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
         }
         return dp[length - 1];
+//        if (nums == null || nums.length == 0) {
+//            return 0;
+//        }
+//        int length = nums.length;
+//        if (length == 1) {
+//            return nums[0];
+//        }
+//        int[] dp = new int[length];
+//        dp[0] = nums[0];
+//        dp[1] = Math.max(nums[0], nums[1]);
+//        for (int i = 2; i < length; i++) {
+//            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+//        }
+//        return dp[length - 1];
     }
 
     //滚动数组 + 动态规划
