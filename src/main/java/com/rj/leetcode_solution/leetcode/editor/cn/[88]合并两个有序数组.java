c4b8252fc92813957c6d82d370cb1c0f9e09b3ab
jiaxianyang <jiaxianyang@jd.com>
2024-05-30 14:48:35 +0800

    feat: [88]合并俩个有序数组

--- src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[88]合并两个有序数组.java
+++ src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[88]合并两个有序数组.java
@@ -46,63 +46,78 @@ class P88MergeSortedArray {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public void merge(int[] nums1, int m, int[] nums2, int n) {
-            //1.时间复杂度暴力
-//            for (int i = 0; i < n; i++) {
-//                nums1[m + i] = nums2[i];
-//            }
-//            Arrays.sort(nums1);
+            //1、合并后直接排序
+//            merge1(nums1, m, nums2, n);
+            //2、双指针
+//            merge2(nums1, m, nums2, n);
+            //3、逆向双指针
+//            merge3(nums1, m, nums2, n);
+            //4、逆向简洁版本
+//            merge4(nums1, m, nums2, n);
+            //5、while循环
+            merge5(nums1, m, nums2, n);
+        }
 
-            //2.双指针
-//            int p1 = 0, p2 = 0;
-//            int[] sorted = new int[m + n];
-//            int cur;
-//            while (p1 < m || p2 < n) {
-//                if (p1 == m) {
-//                    cur = nums2[p2++];
-//                } else if (p2 == n) {
-//                    cur = nums1[p1++];
-//                } else if (nums1[p1] < nums2[p2]) {
-//                    cur = nums1[p1++];
-//                } else {
-//                    cur = nums2[p2++];
-//                }
-//                sorted[p1 + p2 - 1] = cur;
-//            }
-//            for (int i = 0; i < m + n; i++) {
-//                nums1[i] = sorted[i];
-//            }
+        //1、直接合并后排序
+        private void merge1(int[] nums1, int m, int[] nums2, int n) {
+            for (int i = 0; i < n; i++) {
+                nums1[m + i] = nums2[i];
+            }
+            Arrays.sort(nums1);
+        }
 
-            //3.逆向双指针
-//            int p1 = m - 1, p2 = n - 1;
-//            int tail = m + n - 1;
-//            int cur;
-//            while (p1 > -1 || p2 > -1) {
-//                if (p1 == -1) {
-//                    cur = nums2[p2--];
-//                } else if (p2 == -1) {
-//                    cur = nums1[p1--];
-//                } else if (nums1[p1] < nums2[p2]) {
-//                    cur = nums2[p2--];
-//                } else {
-//                    cur = nums1[p1--];
-//                }
-//                nums1[tail--] = cur;
-//            }
+        //2、双指针
+        private void merge2(int[] nums1, int m, int[] nums2, int n) {
+           int p1 = 0, p2 = 0, len = 0;
+            int[] sorted = new int[m + n];
+            while (p1 < m || p2 < n) {
+                if (p1 == m) {
+                    sorted[len++] = nums2[p2++];
+                } else if (p2 == n) {
+                    sorted[len++] = nums1[p1++];
+                } else if (nums1[p1] < nums2[p2]) {
+                    sorted[len++] = nums1[p1++];
+                } else {
+                    sorted[len++] = nums2[p2++];
+                }
+            }
+            for (int i = 0; i < (m + n); i++) {
+                nums1[i] = sorted[i];
+            }
+        }
 
-            //4
-//            int len1 = m - 1;
-//            int len2 = n - 1;
-//            int len = m + n - 1;
-//
-//            while(len1 >= 0 && len2 >= 0) {
-//                // 注意--符号在后面，表示先进行计算再减1，这种缩写缩短了代码
-//                nums1[len--] = nums1[len1] > nums2[len2] ? nums1[len1--] : nums2[len2--];
-//            }
-//            // 表示将nums2数组从下标0位置开始，拷贝到nums1数组中，从下标0位置开始，长度为len2+1
-//            //只要len2 有值都是小于 nums1 的值，并且nums1所有的值都放在数组后面了
-//            System.arraycopy(nums2, 0, nums1, 0, len2 + 1);
+        //3、逆向双指针
+        private void merge3(int[] nums1, int m, int[] nums2, int n) {
+            int p1 = m - 1, p2 = n - 1;
+            int tail =  m + n - 1;
+            while (p1 > -1 || p2 > -1) {
+                if (p1 == -1) {
+                    nums1[tail--] = nums2[p2--];
+                } else if (p2 == -1) {
+                    nums1[tail--] = nums1[p1--];
+                } else if (nums1[p1] < nums2[p2]) {
+                    nums1[tail--] = nums2[p2--];
+                } else {
+                    nums1[tail--] = nums1[p1--];
+                }
+            }
+        }
 
-            // 方法  5 while循环
+        //4、逆向双指针简洁版
+        private void merge4(int[] nums1, int m, int[] nums2, int n) {
+            int len1 = m - 1;
+            int len2 = n - 1;
+            int len = m + n - 1;
+            while (len1 >= 0 && len2 >= 0) {
+                // 注意--符号在后面，表示先进行计算再减1，这种缩写缩短了代码
+                nums1[len--] = nums1[len1] > nums2[len2] ? nums1[len1--] : nums2[len2--];
+            }
+            // 表示将nums2数组从下标0位置开始，拷贝到nums1数组中，从下标0位置开始，长度为len2 + 1
+            //只要len2 有值都是小于 nums1 的值，并且nums1所有的值都放在数组后面了
+            System.arraycopy(nums2, 0, nums1, 0, len2 + 1);
+        }
+
+        private void merge5(int[] nums1, int m, int[] nums2, int n) {
             int i = 0, j = 0, k = 0;
             int[] result = new int[m + n];
             while (i < m && j < n) {
