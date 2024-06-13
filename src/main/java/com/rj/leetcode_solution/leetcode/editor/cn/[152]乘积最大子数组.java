6997bc27a236b57093144e8ae9f28169b678b4ba
jiaxianyang <jiaxianyang@jd.com>
2024-06-13 16:49:06 +0800

    feat: [152] 乘机最大子数组

--- src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[152]乘积最大子数组.java
+++ src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[152]乘积最大子数组.java
@@ -42,23 +42,31 @@ class P152MaximumProductSubarray{
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int maxProduct(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
         int length = nums.length;
-        int[] maxF = new int[length];
-        int[] minF = new int[length];
-        maxF[0] = nums[0];
-        minF[0] = nums[0];
+        int[] max = new int[length];
+        int[] min = new int[length];
+
+        max[0] = nums[0];
+        min[0] = nums[0];
+        int ans = nums[0];
+
         for (int i = 1; i < length; i++) {
-            //子问题: 当前最后一个元素看，前面的最大值 * 当前元素， 当前元素 ， 最小值 * 当面元素 取最大值，一定是连续乘积的最大值
-            maxF[i] = Math.max(maxF[i - 1] * nums[i], Math.max(nums[i], minF[i - 1] * nums[i]));
-            //子问题: 当前最后一个元素看，前面的最小值 * 当前元素， 当前元素， 最大值 * 当前元素（正负得负数），一定是当前连续数组的最小乘积
-            minF[i] = Math.min(minF[i - 1] * nums[i], Math.min(nums[i], maxF[i - 1] * nums[i]));
+            if (nums[i] >= 0) {
+                max[i] = Math.max(nums[i], nums[i] * max[i - 1]);
+                min[i] = Math.min(nums[i], nums[i] * min[i - 1]);
+            } else {
+                max[i] = Math.max(nums[i], nums[i] * min[i - 1]);
+                min[i] = Math.min(nums[i], nums[i] * max[i - 1]);
+            }
+            ans = Math.max(ans, max[i]);
         }
-        int ans = maxF[0];
-        for (int i = 1; i < length; i++) {
-            ans = Math.max(ans, maxF[i]);
-        }
-        //测试用例有问题，超限了***
-        return ans == 1981284352 ? 1000000000 : ans;
+        return ans;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
