faec8f17419ec0aea3e440c262b35854b7d24ec2
jiaxianyang <jiaxianyang@jd.com>
2024-07-03 21:13:57 +0800

    feat: [212] 单词搜索2

---
+++ src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[212]单词搜索II.java
@@ -0,0 +1,115 @@
+//给定一个 m x n 二维字符网格 board 和一个单词（字符串）列表 words， 返回所有二维网格上的单词 。 
+//
+// 单词必须按照字母顺序，通过 相邻的单元格 内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母在一个单词中不允许被重复使
+//用。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：board = [["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f",
+//"l","v"]], words = ["oath","pea","eat","rain"]
+//输出：["eat","oath"]
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：board = [["a","b"],["c","d"]], words = ["abcb"]
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// m == board.length 
+// n == board[i].length 
+// 1 <= m, n <= 12 
+// board[i][j] 是一个小写英文字母 
+// 1 <= words.length <= 3 * 10⁴ 
+// 1 <= words[i].length <= 10 
+// words[i] 由小写英文字母组成 
+// words 中的所有字符串互不相同 
+// 
+//
+// Related Topics 字典树 数组 字符串 回溯 矩阵 👍 870 👎 0
+
+package com.rj.leetcode_solution.leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+//java:单词搜索 II
+class P212WordSearchIi{
+    public static void main(String[] args){
+        Solution solution = new P212WordSearchIi().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    public List<String> findWords(char[][] board, String[] words) {
+        Trie trie = new Trie();
+        for (String word : words) {
+            trie.insert(word);
+        }
+        Set<String> ans = new HashSet<>();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                dfs(board, trie, i, j, ans);
+            }
+        }
+        return new ArrayList<>(ans);
+    }
+
+        private void dfs(char[][] board, Trie now, int i, int j, Set<String> ans) {
+            if (!now.children.containsKey(board[i][j])) {
+                return;
+            }
+            char ch = board[i][j];
+            now = now.children.get(ch);
+            if (!"".equals(now.word)) {
+                ans.add(now.word);
+            }
+            board[i][j] = '#';
+            for (int[] dir : dirs) {
+                int i2 = i + dir[0], j2 = j + dir[1];
+                if (i2 >= 0 && i2 < board.length && j2 >= 0 && j2 < board[0].length) {
+                    dfs(board, now, i2, j2, ans);
+                }
+            }
+            board[i][j] = ch;
+        }
+    }
+
+    class Trie {
+        String word;
+        Map<Character, Trie> children;
+        boolean isWord;
+
+        public Trie() {
+            this.word = "";
+            this.children = new HashMap<>();
+        }
+
+        public void insert(String word) {
+            Trie cur = this;
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+                if (!cur.children.containsKey(c)) {
+                    cur.children.put(c, new Trie());
+                }
+                cur = cur.children.get(c);
+            }
+            cur.word = word;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
