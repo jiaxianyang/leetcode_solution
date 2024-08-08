b877687359259ac5125f088bf02de260023f1fc4
jiaxianyang <jiaxianyang@jd.com>
2024-08-08 18:58:53 +0800

    feat: [200] 并查集解决岛屿与数量

--- src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[200]岛屿数量.java
+++ src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[200]岛屿数量.java
@@ -55,7 +55,18 @@ class P200NumberOfIslands {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+
+        /**
+         * 入口
+         */
         public int numIslands(char[][] grid) {
+            // 并查集
+//            return numsIsLandsByUnion(grid);
+            // dfs
+            return numsIsLandsByDfs(grid);
+        }
+
+        public int numsIsLandsByDfs(char[][] grid) {
             if (grid == null || grid.length == 0) {
                 return 0;
             }
@@ -84,6 +95,78 @@ class P200NumberOfIslands {
             dfs(grid, r + 1, c);
             dfs(grid, r, c - 1);
             dfs(grid, r, c + 1);
+        }
+
+
+        class UnionFind {
+            int count;
+            int[] parent;
+
+            public UnionFind(char[][] grid) {
+                count = 0;
+                int m = grid.length;
+                int n = grid[0].length;
+                parent = new int[m * n];
+                for (int i = 0; i < m; i++) {
+                    for (int j = 0; j < n; j++) {
+                        if (grid[i][j] == '1') {
+                            parent[i * n + j] = i * n + j;
+                            count++;
+                        }
+                    }
+                }
+            }
+
+            public int find(int i) {
+                while (i != parent[i]) {
+                    parent[i] = parent[parent[i]];
+                    i = parent[i];
+                }
+                return i;
+            }
+
+            public void union(int p, int q) {
+                int rootP = find(p);
+                int rootQ = find(q);
+                if (rootP == rootQ) {
+                    return;
+                }
+                parent[rootP] = rootQ;
+                count--;
+            }
+
+            public int getCount() {
+                return count;
+            }
+        }
+
+        private int numsIsLandsByUnion(char[][] grid) {
+            if (grid == null || grid.length == 0) {
+                return 0;
+            }
+            int nr = grid.length;
+            int nc = grid[0].length;
+            UnionFind uf = new UnionFind(grid);
+            for (int r = 0; r < nr; r++) {
+                for (int c = 0; c < nc; c++) {
+                    if (grid[r][c] == '1') {
+                        grid[r][c] = '0';
+                        if (r - 1 >= 0 && grid[r - 1][c] == '1') {
+                            uf.union(r * nc + c, (r - 1) * nc + c);
+                        }
+                        if (r + 1 < nr && grid[r + 1][c] == '1') {
+                            uf.union(r * nc + c, (r + 1) * nc + c);
+                        }
+                        if (c - 1 >= 0 && grid[r][c - 1] == '1') {
+                            uf.union(r * nc + c, r * nc + c - 1);
+                        }
+                        if (c + 1 < nc && grid[r][c + 1] == '1') {
+                            uf.union(r * nc + c, r * nc + c + 1);
+                        }
+                    }
+                }
+            }
+            return uf.getCount();
         }
 
     }
