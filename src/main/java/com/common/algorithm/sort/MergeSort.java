cfdbabe756d1b49917e7e8d20372e55dbb2141c9
jiaxianyang <jiaxianyang@jd.com>
2025-02-24 20:21:37 +0800

    feat: 归并排序

--- src/main/java/com/common/algorithm/sort/MergeSort.java
+++ src/main/java/com/common/algorithm/sort/MergeSort.java
@@ -3,7 +3,14 @@ package com.common.algorithm.sort;
 /**
  * MergeSort简介
  * <p>
- * 归并排序
+ * 归并排序- 分治
+ * 1、把长度为n的输入序列分成俩个长度为n/2的子序列
+ * 2、对这俩个子序列分别采取归并排序
+ * 3、将俩个排序好的子序列合并成一个最终的排序序列
+ *
+ * 归并 和 快排具有相似性，但步骤顺序相反
+ * 归并：先排序左右子数组，然后合并俩个有序子数组
+ * 快排：先调配出左右子数组，然后对于左右子数据进行排序。
  *
  * @author jiaxianyang
  * @date 2021-11-12 11:39
@@ -27,17 +34,17 @@ public class MergeSort extends ArraySortHandler implements ArraySort {
         if (right <= left) {
             return;
         }
-        int mid = (left + right) >> 1; // (left + right) / 2
+        int mid = (left + right) / 2;
         mergeSort(array, left, mid);
         mergeSort(array, mid + 1, right);
-        merge(array, left, mid, right);
+        mergeArray(array, left, mid, right);
     }
 
-    private static void merge(int[] array, int left, int mid, int right) {
+    private static void mergeArray(int[] array, int left, int mid, int right) {
         int[] temp = new int[right - left + 1];
         int i = left;
         int j = mid + 1;
-        int k = 0;
+        int k  = 0;
         while (i <= mid && j <= right) {
             temp[k++] = array[i] <= array[j] ? array[i++] : array[j++];
         }
@@ -50,7 +57,6 @@ public class MergeSort extends ArraySortHandler implements ArraySort {
 //        for (int l = 0; l < temp.length; l++) {
 //            array[left + l] = temp[l];
 //        }
-        //jdk 提供的数组copy
         System.arraycopy(temp, 0, array, left, temp.length);
     }
 
