9c6e6bb8e76e4832b51876359a6c3f7c61c3115a
jiaxianyang <jiaxianyang@jd.com>
2025-03-11 22:55:30 +0800

    feat: 5 最长回文子串

---
+++ src/main/java/com/rj/leetcode_solution/leetcode/editor/cn/[5]最长回文子串.java
@@ -0,0 +1,78 @@
+//给你一个字符串 s，找到 s 中最长的 回文 子串。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "babad"
+//输出："bab"
+//解释："aba" 同样是符合题意的答案。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = "cbbd"
+//输出："bb"
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 1000 
+// s 仅由数字和英文字母组成 
+// 
+//
+// Related Topics 双指针 字符串 动态规划 👍 7540 👎 0
+
+package com.rj.leetcode_solution.leetcode.editor.cn;
+//java:最长回文子串
+class P5LongestPalindromicSubstring{
+    public static void main(String[] args){
+        Solution solution = new P5LongestPalindromicSubstring().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
+        int maxLen = 1;
+        boolean[][] dp = new boolean[len][len];
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+        //核心思想，就是如果当前是dp[i + 1][j - 1]是回文子串，那么如果array[i] == array[j] 那么，dp[i][j] 也是回文子串
+        int begin = 0;
+        char[] charArray = s.toCharArray();
+        for (int L = 2; L <= len; L++) {
+            for (int i = 0; i < len; i++) {
+                int j = L + i - 1;
+                if (j >= len) {
+                    break;
+                }
+                if (charArray[i] != charArray[j]) {
+                    dp[i][j] = false;
+                } else {
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1;
+                    begin = i;
+                }
+            }
+        }
+        return s.substring(begin, begin + maxLen);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
